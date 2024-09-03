0bf8f632be0d4dbf542682a80fe593c9bbb1df40
Chad Byous <chadpb199@gmail.com>
2024-09-03 14:10:29 -0500

    Finish testing.py functionality

--- testing.py
+++ testing.py
@@ -1,25 +1,60 @@
 import sqlite3 as sql
-import argparse
+import argparse as argp
 
 con = sql.connect("ciu_stats.db")
 cur = con.cursor()
 
-
+parser = argp.ArgumentParser(
+    prog="testing.py",
+    description="""A script for adding or clearing test data to or from the 
+        sqlite database for CIU-Stats"""
+    )
+    
+parser.add_argument(
+    "-c",
+    "--clear",
+    action="store_true",
+    help="clear all data from the database"
+    )
+parser.add_argument(
+    "-a",
+    "--add",
+    type=int,
+    help="add N rows of test data to the database (default 5)",
+    const=5,
+    metavar="N",
+    nargs="?",
+    
+    )
 
 def clear_database():
     
     cur.execute("""DELETE FROM stats""")
     
+    con.commit()
+    
+    print("All rows cleared from database.")
+    
 def add_testing_data(rows:int=5):
     
+    try:
+        last_row = int(cur.execute("""SELECT max(CRN) from stats"""
+            ).fetchone()[0][0])
+            
+    except TypeError:
+        last_row = -1
+        
+    new_row = last_row+1
+        
     test_data = []
     cols = 8
     
-    for r in range(rows):
-        for c in range(cols):            
-            test_data.append(f"{r}{c}")
-        
-    test_data = [[i for i in test_data if i[0] == str(r)] for r in range(rows)]
+    for r in range(new_row, new_row+rows):
+        row = []
+        for c in range(cols):
+            row.append(f"{r}{c}")
+            if len(row) == 8:
+                test_data.append(row)
     
     cur.executemany("""
         INSERT INTO stats
@@ -27,9 +62,19 @@ def add_testing_data(rows:int=5):
         test_data
         )
         
+    con.commit()
+    
+    print(f"{rows} rows added to database.")
+        
         
 
 if __name__ == "__main__":
-    clear_database()
     
-    add_testing_data()
+    args = vars(parser.parse_args())
+    
+    if args["clear"]:
+        clear_database()
+    
+    if args["add"] != None:
+        add_testing_data(args["add"])
+    
